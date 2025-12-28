bc394e6fcc7d4d85bce6707708007c470963f2ba
pirosapphic <rocchietti.march.paolo@gmail.com>
2025-12-28 19:54:57 +0100

    Begun to implement basic classes
    implemented the class 'object' for the default celestial body, unsure
    about some decisions (read comments), the goal is to implement
    derived classes star and planet: planet should have a member to indicate
    which star it orbits around.
    Maybe will implement a similar class (moon) for objects orbiting planets

--- include/solarsystem.h
+++ include/solarsystem.h
@@ -1,7 +1,28 @@
 #ifndef solarsystem_h
 #define solarsystem_h
+#include<string>
 //alternatively
 //#pragma once
 //!without endif!
+
+class object{
+  public:
+    void printinfo();
+    object(std::string name, double mass ,double x, double y, double z, double vx, double vy, double vz); //default constructor
+    object(std::string name, double mass, double* pos, double* vel); //array constructor
+    ~object(); //destructor
+  private: //or protected???
+    std::string m_name;
+    double m_mass;
+    double m_x;
+    double m_y;
+    double m_z;
+    double m_vx;
+    double m_vy;
+    double m_vz;
+};
+
+class star: public object{
+};
 
 #endif
