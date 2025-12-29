d9811b24bc9e1c9cae412c10a5564ea3ddfcc793
pirosapphic <rocchietti.march.paolo@gmail.com>
2025-12-29 12:08:30 +0100

    Begun to implement basic classes (COPY OF PREVIOUS COMMIT)
    implemented the class 'object' for the default celestial body, unsure
    about some decisions (read comments), the goal is to implement
    derived classes star and planet: planet should have a member to indicate
    which star it orbits around.
    Maybe will implement a similar class (moon) for objects orbiting planets

--- include/solarsystem.cc
+++ include/solarsystem.cc
@@ -2,3 +2,32 @@
 #include<iostream>
 #include<string>
 
+object::object(std::string name, double mass ,double x, double y, double z, double vx, double vy, double vz):
+  m_name(name),
+  m_mass(mass),
+  m_x(x),
+  m_y(y),
+  m_z(z),
+  m_vx(vx),
+  m_vy(vy),
+  m_vz(vz)
+{}; //default constructor
+
+object::object(std::string name, double mass, double* pos, double* vel):
+  m_name(name),
+  m_mass(mass),
+  m_x(pos[0]),
+  m_y(pos[1]),
+  m_z(pos[2]),
+  m_vx(vel[0]),
+  m_vy(vel[1]),
+  m_vz(vel[2])
+{}; //array constructor
+
+
+
+void object::printinfo(){
+  std::cout << "Position of "<<m_name<<": ("<<m_x<<", "<<m_y<<", "<<m_z<<")m\n";
+  std::cout << "Velocity of "<<m_name<<": ("<<m_vx<<", "<<m_vy<<", "<<m_vz<<")m/s\n";
+};
+
--- include/solarsystem.h
+++ include/solarsystem.h
@@ -4,4 +4,25 @@
 //alternatively
 //#pragma once
 //!without endif!
+
+class object{
+  public:
+    void printinfo();
+    object(std::string name, double mass ,double x, double y, double z, double vx, double vy, double vz); //default constructor
+    object(std::string name, double mass, double* pos, double* vel); //array constructor
+    ~object(); //destructor
+  private: //or protected???
+    std::string m_name;
+    double m_mass;
+    double m_x;
+    double m_y;
+    double m_z;
+    double m_vx;
+    double m_vy;
+    double m_vz;
+};
+
+class star: public object{
+};
+
 #endif
