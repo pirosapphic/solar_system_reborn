e5c4052cd8e020d4f19d0e0fbab0effc5493e56f
0universe0 <0universe0@protonmail.com>
2025-12-29 18:53:52 +0100

    implementation of class CelestialBody, getters, setters

--- include/solarsystem.cc
+++ include/solarsystem.cc
@@ -1,4 +1,55 @@
-#include "solarsystem.h"
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <string>
 
+#include "solarsystem.h"
+
+CelestialBody::CelestialBody (std::string name, double mass, std::vector<double> position) {
+    m_mass = mass;
+    m_position = position;
+}
+
+void CelestialBody::printState() {
+    std::cout << "The state of " << m_name << " is:\n";
+    std::cout << "  mass: " << m_mass << "\n";
+    std::cout << "  position: (" << m_position[0] << ", " << m_position[1] << ")\n";
+}
+
+// getters
+
+double CelestialBody::getMass() {
+    return m_mass;
+}
+
+double CelestialBody::getX() {
+    return m_position[0];
+}
+double CelestialBody::getY() {
+    return m_position[1];
+}    
+
+std::vector<double> CelestialBody::getPos() {
+    return m_position;
+}
+
+// setters
+
+void CelestialBody::setName(std::string new_name) {
+    m_name = new_name;
+}
+
+void CelestialBody::setMass(double new_mass) {
+    m_mass = new_mass;
+}
+
+void CelestialBody::setX(double new_x) {
+    m_position[0] = new_x;
+}
+
+void CelestialBody::setY(double new_y) {
+    m_position[1] = new_y;
+}
+
+void CelestialBody::setPos(std::vector<double> new_pos) {
+    m_position = new_pos;
+}
--- include/solarsystem.h
+++ include/solarsystem.h
@@ -1,7 +1,59 @@
 #ifndef solarsystem_h
 #define solarsystem_h
-#include<string>
+#include <iostream>
+#include <vector>
+#include <string>
+
 //alternatively
 //#pragma once
 //!without endif!
+
+
+// The following convention is wise to be followed:
+// naming prefix:
+//      m for members
+//      p for pointers (pp for pointers to pointers) 
+//      i for indexes and iterators (i++) 
+//      ...
+
+
+// The following naming convention is wise to be followed too (if you want to stay alive...):
+// naming convention:
+//      variables are to be named meaningfully, write a name that explains what the "x" is (for this specific situation snake_case is preferred over camelCase): mass, width, moon_orbit... 
+//      functions and methods are to be named starting with a lower case letter: exampleMethod()
+//      constants are all upper case: PI=3.141...
+//      types (classes or whatever) start with uppercase letters and follow CamelCase convention: CelestialBody (see later)
+//
+// Other rules are free to be added. Last but not least:
+// 
+// whoever will add "using namespace std" at the start of ANY file will be mocked, bullied, prosecuted and publicly punished by me (0universe0). Have a great time coding ;)
+
+class CelestialBody {
+
+    public:
+        CelestialBody (std::string name, double mass, std::vector<double> position);
+        ~CelestialBody () {};
+
+        void printState();
+
+        // getters
+        std::string getName();
+        double getMass(); 
+        double getX();
+        double getY();
+        std::vector<double> getPos();
+     
+        // setters
+        void setName(std::string new_name);
+        void setMass(double new_mass);
+        void setX(double new_x);
+        void setY(double new_y);
+        void setPos(std::vector<double> new_pos); 
+         
+    private:
+        std::string m_name;
+        double m_mass;
+        std::vector<double> m_position;
+}; 
+
 #endif
